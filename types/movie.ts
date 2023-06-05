81028cbd307c9e39d3f9a2bd6c6a19b1ffd963d8
nehalhassan <nh.elsaify@gmail.com>
2023-06-05 19:23:18 +0300

    feat: add types for movie and movies list

---
+++ types/movie.ts
@@ -0,0 +1,46 @@
+type MovieList = {
+  dates: { maximum: string; minimum: string };
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+};
+
+interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: false;
+  vote_average: number;
+  vote_count: number;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string;
+    backdrop_path: string;
+  };
+  budget: number;
+  genres: { id: number; name: string }[];
+  homepage: string;
+  imdb_id: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: { iso_3166_1: string; name: string }[];
+  revenue: number;
+  runtime: number;
+  spoken_languages: { english_name: string; iso_639_1: string; name: string }[];
+  status: string;
+  tagline: string;
+}
