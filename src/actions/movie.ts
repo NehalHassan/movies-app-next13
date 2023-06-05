611764a041eb88f8d4269b37c8de7723ecc50675
nehalhassan <nh.elsaify@gmail.com>
2023-06-05 19:24:16 +0300

    feat: add action to fetch a movie with id

--- src/actions/movie.ts
+++ src/actions/movie.ts
@@ -1,4 +1,17 @@
 import { customFetch } from "@/src/helpers/helpers/fetch";
+import { Credit } from "@/types/credit";
+
+export async function getMovie(id: string): Promise<Movie> {
+  const res = await customFetch({
+    url: `/movie/${id}?language=en-US`,
+    method: "GET",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
 
 export async function getMovies(
   page = 1,
