ddb049bf8dae1c7be431b9b9f262a298dfb5d004
nehalhassan <nh.elsaify@gmail.com>
2023-06-05 19:26:08 +0300

    feat: add new action for fetching keywords

--- src/actions/movie.ts
+++ src/actions/movie.ts
@@ -13,6 +13,18 @@ export async function getMovie(id: string): Promise<Movie> {
   return res.json();
 }
 
+export async function getKeywords(id: string): Promise<Keywords> {
+  const res = await customFetch({
+    url: `/movie/${id}/keywords`,
+    method: "GET",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
 export async function getMovies(
   page = 1,
   category = "now_playing"
