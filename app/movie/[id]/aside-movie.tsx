ea36de2e2a6a65055a6bd77c82cd843044734b45
nehalhassan <nh.elsaify@gmail.com>
2023-06-05 19:27:30 +0300

    feat: add  mode side bar section

---
+++ app/movie/[id]/aside-movie.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Keywords from "./keywords";
+import { getMovie } from "@/src/actions/movie";
+
+const Info = ({ label, value }: { label: string; value: string }) => (
+  <li className="mb-2">
+    <p className="font-semibold">{label}</p>
+    <span>{value}</span>
+  </li>
+);
+
+async function AsideMovie({ movieId }: { movieId: string }) {
+  const movie = await getMovie(movieId);
+
+  return (
+    <>
+      <ul>
+        <Info label="Status" value={movie.status} />
+        <Info label="Original Language" value={movie.original_language} />
+        <Info label="Budget" value={`$${movie.budget}`} />
+        <Info label="Revenue" value={`$${movie.revenue}`} />
+      </ul>
+      {/* @ts-expect-error Server Component */}
+      <Keywords movieId={movieId} />
+    </>
+  );
+}
+
+export default AsideMovie;
